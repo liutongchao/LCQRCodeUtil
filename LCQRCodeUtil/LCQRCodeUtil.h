6cf119f3696f8e3cc6977a1456bb7214b646487a
liutongchao <413281269@qq.com>
2016-05-05 17:36:36 +0800

    no message

---
+++ LCQRCodeUtil/LCQRCodeUtil.h
@@ -0,0 +1,31 @@
+//
+//  LCQrcodeUtil.h
+//  二维码测试
+//
+//  Created by 刘通超 on 16/5/5.
+//  Copyright © 2016年 刘通超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCQRCodeUtil : NSObject
+/**
+ *  生成二维码图片
+ *
+ *  @param QRString  二维码内容
+ *  @param sizeWidth 图片size（正方形）
+ *  @param color     填充色
+ *
+ *  @return  二维码图片
+ */
++(UIImage *)createQRimageString:(NSString *)QRString sizeWidth:(CGFloat)sizeWidth fillColor:(UIColor *)color;
+
+/**
+ *  读取图片中二维码信息
+ *
+ *  @param image 图片
+ *
+ *  @return 二维码内容
+ */
++(NSString *)readQRCodeFromImage:(UIImage *)image;
+@end
