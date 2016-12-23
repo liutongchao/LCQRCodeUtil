46851c1f52b4ddfd6067d05bd012e680dc98e4dc
liutongchao <413281269@qq.com>
2016-12-23 10:28:49 +0800

    添加扫描页面透明框，设置扫描区域

--- LCQRcodeScan/LCQRcodeScan/ViewController.m
+++ LCQRcodeScan/LCQRcodeScan/ViewController.m
@@ -9,12 +9,32 @@
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+/**
+ *  屏幕 高 宽 边界
+ */
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define SCREEN_WIDTH  [UIScreen mainScreen].bounds.size.width
+#define SCREEN_BOUNDS  [UIScreen mainScreen].bounds
+
+#define TOP (SCREEN_HEIGHT-220)/2
+#define LEFT (SCREEN_WIDTH-220)/2
+
+#define kScanRect CGRectMake(LEFT, TOP, 220, 220)
+
+@interface ViewController ()<AVCaptureMetadataOutputObjectsDelegate>{
+    int num;
+    BOOL upOrdown;
+    NSTimer * timer;
+    CAShapeLayer *cropLayer;
+}
 @property (strong,nonatomic)AVCaptureDevice * device;
 @property (strong,nonatomic)AVCaptureDeviceInput * input;
 @property (strong,nonatomic)AVCaptureMetadataOutput * output;
 @property (strong,nonatomic)AVCaptureSession * session;
 @property (strong,nonatomic)AVCaptureVideoPreviewLayer * preview;
+
+@property (nonatomic, strong) UIImageView * line;
+
 @end
 
 @implementation ViewController
@@ -22,11 +42,80 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self setupCamera];
+    
+    [self configView];
+}
+
+-(void)configView{
+    UIImageView * imageView = [[UIImageView alloc]initWithFrame:kScanRect];
+    imageView.image = [UIImage imageNamed:@"pick_bg"];
+    [self.view addSubview:imageView];
+    
+    upOrdown = NO;
+    num =0;
+    _line = [[UIImageView alloc] initWithFrame:CGRectMake(LEFT, TOP+10, 220, 2)];
+    _line.image = [UIImage imageNamed:@"line.png"];
+    [self.view addSubview:_line];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:.02 target:self selector:@selector(animation1) userInfo:nil repeats:YES];
+
+}
+-(void)viewWillAppear:(BOOL)animated{
+
+    [self setCropRect:kScanRect];
+    
+    [self performSelector:@selector(setupCamera) withObject:nil afterDelay:0.3];
+
+}
+
+-(void)animation1
+{
+    if (upOrdown == NO) {
+        num ++;
+        _line.frame = CGRectMake(LEFT, TOP+10+2*num, 220, 2);
+        if (2*num == 200) {
+            upOrdown = YES;
+        }
+    }
+    else {
+        num --;
+        _line.frame = CGRectMake(LEFT, TOP+10+2*num, 220, 2);
+        if (num == 0) {
+            upOrdown = NO;
+        }
+    }
+    
+}
+
+
+- (void)setCropRect:(CGRect)cropRect{
+    cropLayer = [[CAShapeLayer alloc] init];
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, nil, cropRect);
+    CGPathAddRect(path, nil, self.view.bounds);
+    
+    [cropLayer setFillRule:kCAFillRuleEvenOdd];
+    [cropLayer setPath:path];
+    [cropLayer setFillColor:[UIColor blackColor].CGColor];
+    [cropLayer setOpacity:0.6];
+    
+    
+    [cropLayer setNeedsDisplay];
+    
+    [self.view.layer addSublayer:cropLayer];
 }
 
 - (void)setupCamera
 {
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device==nil) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"设备没有摄像头" preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+        }]];
+        [self presentViewController:alert animated:YES completion:nil];
+        return;
+    }
     // Device
     _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
@@ -37,6 +126,15 @@
     _output = [[AVCaptureMetadataOutput alloc]init];
     [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
     
+    //设置扫描区域
+    CGFloat top = TOP/SCREEN_HEIGHT;
+    CGFloat left = LEFT/SCREEN_WIDTH;
+    CGFloat width = 220/SCREEN_WIDTH;
+    CGFloat height = 220/SCREEN_HEIGHT;
+    ///top 与 left 互换  width 与 height 互换
+    [_output setRectOfInterest:CGRectMake(top,left, height, width)];
+
+   
     // Session
     _session = [[AVCaptureSession alloc]init];
     [_session setSessionPreset:AVCaptureSessionPresetHigh];
@@ -72,15 +170,27 @@
     {
         //停止扫描
         [_session stopRunning];
+        [timer setFireDate:[NSDate distantFuture]];
         
         AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex:0];
         stringValue = metadataObject.stringValue;
-        NSLog(stringValue);
+        NSLog(@"扫描结果：%@",stringValue);
         
         NSArray *arry = metadataObject.corners;
         for (id temp in arry) {
             NSLog(@"%@",temp);
         }
+        
+        
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"扫描结果" message:stringValue preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            if (_session != nil && timer != nil) {
+                [_session startRunning];
+                [timer setFireDate:[NSDate date]];
+            }
+
+        }]];
+        [self presentViewController:alert animated:YES completion:nil];
         
     } else {
         NSLog(@"无扫描信息");
